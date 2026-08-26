d2c8c6d31d424a55714ada884a3c0e62839ff2df
victoria122-rgb <vobando741@gmail.com>
2026-08-25 20:48:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,24 +19,24 @@ if modulos == "Introducion variables":
 elif modulos == "Funciones":
     def calcular_caudal_vogel(caudal_maximo=1000, presion_yacimeinto=3000, presion_fondo=200, decimales=2):
     """
-    Calcula el caudal de petróleo utilizando el modelo de Vogel.
-  
-    Parámetros:
-    caudal_maximo (float): Caudal máximo teórico del pozo, BPD.
-    presion_yacimeinto (float): Presión de yacimiento, psi.
-    presion_fondo (float): Presión de fondo fluyente, psi.
-    decimales (int): Número de decimales del resultado.
-  
-    Retorna:
-    float: Caudal estimado de petróleo, BPD.
+        Calcula el caudal de petróleo utilizando el modelo de Vogel.
+      
+        Parámetros:
+        caudal_maximo (float): Caudal máximo teórico del pozo, BPD.
+        presion_yacimeinto (float): Presión de yacimiento, psi.
+        presion_fondo (float): Presión de fondo fluyente, psi.
+        decimales (int): Número de decimales del resultado.
+      
+        Retorna:
+        float: Caudal estimado de petróleo, BPD.
     """
       
     relacion_presion = presion_fondo/presion_yacimeinto
     caudal = caudal_max*(1-0.2*relacion_presion-0.8*(relacion_presion**2))
     return round(caudal, decimales)
   
-    caudal_maximo = st.number_input["Ingrese el caudal maximo"]
-    presion_yacimeinto = st.number_input["Ingrese presion de yacimeinto"]
-    presion_fondo = st.number_input["Ingrese la presion de fondo"]
-    decimales = st.number_input["Ingrese decimales para su calculo"]
+caudal_maximo = st.number_input["Ingrese el caudal maximo"]
+presion_yacimeinto = st.number_input["Ingrese presion de yacimeinto"]
+presion_fondo = st.number_input["Ingrese la presion de fondo"]
+decimales = st.number_input["Ingrese decimales para su calculo"]
 
