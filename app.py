6c35ac86671efcb2127a34a8b8d7a828cfc5b216
victoria122-rgb <vobando741@gmail.com>
2026-08-26 11:18:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ elif modulos == "Funciones":
             float: Caudal estimado de petróleo, BPD.
         """
           
-    relacion_presion = presion_fondo/presion_yacimeinto
-    caudal = caudal_maximo*(1-0.2*relacion_presion-0.8*(relacion_presion**2))
+        relacion_presion = presion_fondo/presion_yacimeinto
+        caudal = caudal_maximo*(1-0.2*relacion_presion-0.8*(relacion_presion**2))
         return round(caudal, decimales)
 
     caudal_maximo = st.number_input("Ingrese el caudal maximo")
