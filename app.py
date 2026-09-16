c7d9cef7006bad9f6db619c82851f240fec1f47f
victoria122-rgb <vobando741@gmail.com>
2026-09-16 13:27:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,12 +35,12 @@ elif modulos == "Funciones":
         caudal = caudal_maximo*(1-0.2*relacion_presion-0.8*(relacion_presion**2))
         return round(caudal, decimales)
 
- caudal_maximo = st.number_input("Ingrese el caudal_maximo", min_value = 0, max_value = 5000, value = 1200)
- presion_yacimiento = st.number_input("Ingrese presion de yacimiento", min_value = 0, max_value = 9000, value = 3000)
- presion_fondo = st.number_input("Ingrese la presion de fondo", min_value = 0, max_value = 9000, value = 1500)
- decimales = st.slider("Ingrese decimales para su calculo", min_value = 0, max_value = 4, value = 2)
- caudal = calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
- st.write("El caudal es:", caudal)
+    caudal_maximo = st.number_input("Ingrese el caudal_maximo", min_value = 0, max_value = 5000, value = 1200)
+    presion_yacimiento = st.number_input("Ingrese presion de yacimiento", min_value = 0, max_value = 9000, value = 3000)
+    presion_fondo = st.number_input("Ingrese la presion de fondo", min_value = 0, max_value = 9000, value = 1500)
+    decimales = st.slider("Ingrese decimales para su calculo", min_value = 0, max_value = 4, value = 2)
+    caudal = calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
+    st.write("El caudal es:", caudal)
 elif modulos == "POO":
         class Pozo:
           def __init__(self, nombre, campo, petroleo, agua):
@@ -60,13 +60,13 @@ elif modulos == "POO":
             produccion_proyectada = (self.petroleo + self.agua) * dias
             return produccion_proyectada
 
-nombre_pozo = st.text_input("Ingrese el nombre del pozo")
-campo_pozo = st.text_input("Ingrese el campo del pozo")
-petroleo = st.number_input("Ingrese produccion de petroleo", min_value = 0, max_value = 5000, value = 1000)
-agua = st.number_input("Ingrese produccion de agua", min_value = 0, max_value = 5000, value = 200)
+    nombre_pozo = st.text_input("Ingrese el nombre del pozo")
+    campo_pozo = st.text_input("Ingrese el campo del pozo")
+    petroleo = st.number_input("Ingrese produccion de petroleo", min_value = 0, max_value = 5000, value = 1000)
+    agua = st.number_input("Ingrese produccion de agua", min_value = 0, max_value = 5000, value = 200)
 
-pozo = pozo(nombre_pozo,campo_pozo,petroleo)
-st.write(pozo.mostrar_informacion())
-st.write(pozo.produccion_total())
-dias = st.number_input("Ingrese los dias a proyectar", min_value = 0, max_value = 365, value = 30)
-st.write(pozo.proyectar_produccion(dias))	
+    pozo = Pozo(nombre_pozo,campo_pozo,petroleo,agua)
+    st.write(pozo.mostrar_informacion())
+    st.write(pozo.produccion_total())
+    dias = st.number_input("Ingrese los dias a proyectar", min_value = 0, max_value = 365, value = 30)
+    st.write(pozo.proyectar_produccion(dias))	
